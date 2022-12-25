f97d0dbcc751587d04bc874e905b89f7fb1847a7
Ruben <rubenvegajusto@gmail.com>
2022-12-25 14:11:12 +0000

    In this commit, I tweak my function code and I learn more about lambda functions

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
     <title>Demo</title>
 </head>
 
-<body><
+<body>
+
+<!--The code from the last commit is not flexible, imagine that instead of filtering by Author, I want to filter by year, would I copy ans paste the function? That's not the best idea at all. So, tweaked my code-->
+
     <?php
 
 //This code will add a filtering feature to the search books feature
@@ -24,29 +27,25 @@
                 'purchaseUrl' => 'http://example.com'
             ],
         ];
-        function filterByAuthor($books, $author) 
-        {
-           $filteredBoooks = [];
-            foreach ($books as $book) 
-            {
-                if ($book ['author'] === $author) 
-                {
-                    $filteredBoooks[] = $book;
-                }
-            }
-                return $filteredBoooks;
-        }
-    ?>
-<!--Initially, I harcoded the code to filter the books ($book['author'] === 'Andy Weir'), but this is unpractical. So I create a function to automate the process-->
+        // This  function filterByAuthor($books, $author) is a name function. I also can create anonymous functions that I can assign to a variable or pass it to another functions. 
+        //I also change the variables names from books to items, author too key and value
+        //Signature is the parameters of the function
+        //fn stands for function and its the standard
+        //array_filter is the prebuilt PHP function
 
-<ul>
-    <?php foreach (filterByAuthor($books, 'Andy Weir') as $book) : ?>
-        <li>
-            <a href="<?= $book ['purchaseUrl'] ?>">
-                <?= $book['name']; ?> (<?= $book ['releaseYear'] ?>) By <?= $book['author']?>
-            </a>
-        </li>
-    <?php endforeach; ?>
-</ul>
+        $filteredBooks = array_filter($books, function ($book) {
+            return $book['author'] === 'Andy Weir';
+        });
+    ?>
+
+    <ul>
+        <?php foreach ($filteredBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
