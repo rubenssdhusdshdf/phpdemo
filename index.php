f3002f43086ddc023d1e4ac74bbff0ff4c43fc83
Ruben <rubenvegajusto@gmail.com>
2022-12-25 12:47:31 +0000

    In this commit, I practiced PHP booleans and conditionals

--- index.php
+++ index.php
@@ -3,35 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+           $message = "You have read $name";
+        } else {
+            $message = "You have NOT read $name";
+        }
+    ?>
 
     <h1>
-        <?php
-            $greeting = "Hello";
-            $noun = "World";
-
-            echo "$greeting $noun" . "!"
-        ?> 
-    </h1>
-
-</body>
-</html>
-index.php
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-</head>
-<body>
-
-    <h1>
-        <?php
-            $greeting = "Hello";
-
-            echo "$greeting Everybody!"
-        ?>
+        <?= $message ?>
     </h1>
 
 </body>
