8758e90988cbee1028b87b7619a63cebafe7ef87
Ruben <rubenvegajusto@gmail.com>
2022-12-25 12:49:17 +0000

    In this commit, I practiced arrays

--- index.php
+++ index.php
@@ -3,31 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
 <body>
-    <?php
-        $name = "Dark Matter";
-        $read = true;
+    <h1>Recommended Books</h1>
 
-        if ($read) {
-           $message = "You have read $name";
-        } else {
-            $message = "You have NOT read $name";
-        }
+    <?php
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
     ?>
 
-    <h1>
-        <?= $message ?>
-    </h1>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
 
 </body>
 </html>
